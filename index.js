5a1bc6873a841f0c5e9859102c0a7c30f8ede4ec
KnifesmithCode <alex.knifesmith@gmail.com>
2018-11-08 15:04:09 -0600

    Change to basic Socket.io server

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+io.on('connection', function(client){
+  client.on('event', function(data){console.log(data)});
+  client.on('disconnect', function(){console.log('User disconnected'});
+});
+
+server.listen(PORT, () => console.log(`Listening with Socket.io on ${PORT}`));
