23699be1d16da029a90449a2a6c545131aa47f94
KnifesmithCode <alex.knifesmith@gmail.com>
2018-11-09 12:37:36 -0600

    Add Express, hopefully

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const server = require('http').createServer();
+const express = require('express');
+const path = require('path');
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+server.listen(PORT, () => console.log(`Listening with Socket.io and Express on ${PORT}`));
+
+app.use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'));
 
 io.on('connection', function(client){
   client.on('event', function(data){console.log(data)});
   client.on('disconnect', function(){console.log('User disconnected'});
 });
-
-server.listen(PORT, () => console.log(`Listening with Socket.io on ${PORT}`));
